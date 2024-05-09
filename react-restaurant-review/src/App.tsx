2d4a128c0e4cf25722623af3db23837b6007f0cb
Andrejp90 <puskasandrej@hotmail.com>
2024-05-09 22:29:59 +0200

    initial commit

---
+++ react-restaurant-review/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <input 
+    type="email" 
+    placeholder="Email address"
+    className="form--input"
+/>
+  )
+}
+
+export default App
